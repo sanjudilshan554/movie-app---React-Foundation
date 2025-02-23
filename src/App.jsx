e30b029cce313fc8f6bb57ed485bcb46d94f4da5
Sanju Dilshan <sanjudilshan554@gmail.com>
2025-02-23 07:34:11 +0530

    Setup API for movies and fetch movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,66 @@
- 
- 
+import Search from "./components/Search";
+import { useState, useEffect } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 function App() {
+  // create useStates
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+      const data = await response.json();
+
+      console.log('data',data)
+    } catch (err) {
+      console.log(`'Error', ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
-    <div className="pattern">
+      <div className="pattern">
+        <div className="wrapper">
+          <img src="./hero.png" alt="hero" />
+          <header>
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without the Hassle
+            </h1>
+            {/* Pass constructor */}
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
 
-      <div className="wrapper">
-        <header>
-          <h1 >Find <span className="text-gradient">Movies</span>  You'll Enjoy Without the Hassle</h1>
-        </header>
+          <section className="all-movies">
+            <h2>All Movies</h2>
 
+            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          </section>
+        </div>
       </div>
-
-    </div>
-  </main>
+    </main>
   );
 }
 
