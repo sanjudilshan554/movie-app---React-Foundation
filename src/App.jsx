3b2d11dcdd1b7ac71e05f656fc3238ddd57017b3
Sanju Dilshan <sanjudilshan554@gmail.com>
2025-02-23 06:20:42 +0530

    tailwind installation and basic css integration with node v20.18.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-const Card = ({ title }) => {
-  return (
-    <div className="card"
-    >
-      <h2>{title}</h2>
-      <button onClick={()=> setHasLiked(true)}>
-        Like
-      </button>
-    </div>
-  );
-};
+ 
+ 
 
 function App() {
-  const [hasLiked, setHasLiked] = useState(false);
-
   return (
-    <div className="card-container">
-      <h2>Functional Arrow Component</h2>
-      <Card title="One" />
-      <Card title="Two" />
-      <Card title="Three" />
+    <main>
+    <div className="pattern">
+
+      <div className="wrapper">
+        <header>
+          <h1 >Find <span className="text-gradient">Movies</span>  You'll Enjoy Without the Hassle</h1>
+        </header>
+
+      </div>
+
     </div>
+  </main>
   );
 }
 
